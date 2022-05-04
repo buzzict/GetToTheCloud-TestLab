f592357c60b8664fff1c956db558a54125fcb2c2
buzzict <info@buzz-ict.nl>
2022-05-04 16:24:36 +0200

    update

--- Set-PowershellRemoting.ps1
+++
@@ -1,7 +0,0 @@
-# Enable WinRM for HTTP rules in Windows Firewall
-Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP" -RemoteAddress Any
-Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP-NoScope" -RemoteAddress Any
-Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP-PUBLIC" -RemoteAddress Any
-
-# Enable PowerShell Remoting forcefully
-Enable-PSRemoting -Force
